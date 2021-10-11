4136206b1e154d28cf9ce1a484715dd7ca57304a
momummim <92283268+momummim@users.noreply.github.com>
2021-10-11 09:44:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Helu Baeeeeeeee',
+    introDesc: `Hôm nay ngày 11/10/2021 và hôm nay bé đã được 22 chủi.`,
+    btnIntro: 'hihihihihihihihi',
+    title: 'Happy Birthday Bé iuuuu 😘😘',
+    desc: 'From Bé Trí with loveeeeee 😘😘',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới có 7 tỷ người mà sao ta gặp đc nhaooo',
+    btnReply: 'Gửi cho bé nà <3',
+    reply: 'chắc do 2 mình có duyên áaaaaa',
+    mess: 'Biết oyyyy 🥰. Thương bé êu nhều 😘😘',
+    messDesc: 'Chiều chiều bé sẽ mua gì mang qua cho nhennnnnnn qua gọi. Chúc bé có 1 sinh nhật thật hạnh phúc',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/tristana.tran1507' 
+}
